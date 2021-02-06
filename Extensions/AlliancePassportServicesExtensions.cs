af8aa0f94f99f2488f80ea2f628d77976f2494c1
Daniel Lozano Navas <d.lozanonavas@outlook.com>
2021-02-06 04:47:24 -0500

    Setup: Default Identity

--- Extensions/AlliancePassportServicesExtensions.cs
+++ Extensions/AlliancePassportServicesExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using FenixAlliance.ABM.Data;
+using FenixAlliance.ACL.Configuration.Enums;
 using FenixAlliance.ACL.Configuration.Interfaces;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureAD.UI;
@@ -7,7 +8,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,95 +22,88 @@ namespace FenixAlliance.APS.Core.Extensions
 
             if (Options.APS.Enable)
             {
-                // Adds required Services for AAD B2C
-                if (Options?.APS?.AzureADB2C?.DefaultProvider ?? false)
-                {
-                    if (!Options?.APS?.AzureAd.DefaultProvider ?? false)
-                    {
-                        // Adds Azure AD B2C Authentication
-                        services.AddAuthentication(o =>
-                        {
-                            o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                            o.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                        })
-                        .AddAzureAdB2C(options =>
-                        {
-                            Configuration.Bind($"APS:{Options.APS.Provider}", options);
-                        })
-                        //.AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options))
-                        //.AddCertificate();
-                        .AddCookie();
 
-                        services.AddAuthorization();
+                if (Options?.APS?.AuthenticationProvider == AuthenticationProvider.DefaultIdentity)
+                {
+
+
+                    services.AddDefaultIdentity<IdentityUser>(options =>
+                        {
+                            options.SignIn.RequireConfirmedAccount = true;
+                        })
+                        .AddEntityFrameworkStores<ABMContext>();
+
+                    services.AddRazorPages();
+
+                    // Adds required services for Default Identity
+                    services.Configure<IdentityOptions>(options =>
+                    {
+                        // Password settings.
+                        options = Options?.APS?.DefaultIdentity?.IdentityOptions ?? new IdentityOptions();
+                    });
+
+                    services.ConfigureApplicationCookie(options =>
+                    {
+
+                        // Cookie settings
+                        options.Cookie.HttpOnly = true;
+
+                        options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                        options.LoginPath = "/Identity/Account/Login";
+                        options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                        options.SlidingExpiration = true;
+
+                    });
+                    // Adds required services for IdentityServer4
+
+                }
+
+                if (Options?.APS?.AuthenticationProvider == AuthenticationProvider.AzureActiveDirectory)
+                {
+                    // Adds required Services for AAD B2C
+                    if (Options?.APS?.AzureAd.DefaultProvider ?? false)
+                    {
+                        if (!Options?.APS?.AzureADB2C?.DefaultProvider ?? false)
+                        {
+                            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+                                .AddAzureAD(options => Configuration.Bind(
+                                    $"APS:{Options.APS.Provider}",
+                                    options)).AddCookie();
+                        }
                     }
                 }
 
-                // Adds required Services for AAD B2C
-                if (Options?.APS?.AzureAd.DefaultProvider ?? false)
+                if (Options?.APS?.AuthenticationProvider == AuthenticationProvider.AzureActiveDirectoryB2C)
                 {
-                    if (!Options?.APS?.AzureADB2C?.DefaultProvider ?? false)
+                    // Adds required Services for AAD B2C
+                    if (Options?.APS?.AzureADB2C?.DefaultProvider ?? false)
                     {
-                        services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
-                            .AddAzureAD(options => Configuration.Bind(
-                                $"APS:{Options.APS.Provider}",
-                                options)).AddCookie();
+                        if (!Options?.APS?.AzureAd.DefaultProvider ?? false)
+                        {
+                            // Adds Azure AD B2C Authentication
+                            services.AddAuthentication(o =>
+                                {
+                                    o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                                    o.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                                })
+                                .AddAzureAdB2C(options =>
+                                {
+                                    Configuration.Bind($"APS:{Options.APS.Provider}", options);
+                                })
+                                //.AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options))
+                                //.AddCertificate();
+                                .AddCookie();
+
+                            services.AddAuthorization();
+                        }
                     }
                 }
 
-
-                services.AddDbContext<ABMContext>(options =>
-                    // options.UseSqlite(
-                    options.UseSqlServer(
-                        Configuration.GetConnectionString("DefaultConnection")));
-
-                services.AddDatabaseDeveloperPageExceptionFilter();
-
-
-                services.AddDefaultIdentity<IdentityUser>(options =>
-                    {
-                        options.SignIn.RequireConfirmedAccount = true;
-                    })
-                    .AddEntityFrameworkStores<ABMContext>();
-
-                services.AddRazorPages();
-
-                // Adds required services for Default Identity
-                services.Configure<IdentityOptions>(options =>
+                if (Options?.APS?.AuthenticationProvider == AuthenticationProvider.IdentityServer4)
                 {
-                    // Password settings.
-                    options.Password.RequireDigit = true;
-                    options.Password.RequireLowercase = true;
-                    options.Password.RequireNonAlphanumeric = true;
-                    options.Password.RequireUppercase = true;
-                    options.Password.RequiredLength = 6;
-                    options.Password.RequiredUniqueChars = 1;
-
-                    // Lockout settings.
-                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                    options.Lockout.MaxFailedAccessAttempts = 5;
-                    options.Lockout.AllowedForNewUsers = true;
-
-                    // User settings.
-                    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                    options.User.RequireUniqueEmail = false;
-                });
-
-                services.ConfigureApplicationCookie(options =>
-                {
-
-                    // Cookie settings
-                    options.Cookie.HttpOnly = true;
-
-                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
-                    options.LoginPath = "/Identity/Account/Login";
-                    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-                    options.SlidingExpiration = true;
-
-                });
-                // Adds required services for IdentityServer4
-
-
+                    // TODO: Finish implementing IdentityServer4
+                }
             }
             #endregion
 
