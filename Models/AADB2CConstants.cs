84fc7b6f9d51de22eee9e619967ce2b1bc6c66bb
Daniel Lozano <d.lozanonavas@outlook.com>
2021-03-06 00:21:13 -0500

    Refactor: auth DTOs / Interfaces to ABM.Data.Access

--- Models/AADB2CConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FenixAlliance.APS.Core.Models
-{
-    public class AADB2CConstants
-    {
-        public const string ScopeUserRead = "User.Read";
-        public const string BearerAuthorizationScheme = "Bearer";
-    }
-}
--- Models/Serialize.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace FenixAlliance.APS.Core.Models
-{
-    public static class Serialize
-    {
-        public static string ToJson(this B2CDecodedToken self) => JsonConvert.SerializeObject(self, AADB2CTokenConverter.Settings);
-    }
-}
